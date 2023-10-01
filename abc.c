9b385125022be83379789ce4d47433a0866daa07
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-09-30 22:21:51 -0700

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+int main()
+{
+   int a,b,c;
+   scanf("%d%d%d",&a,&b,&c);
+   if(a>b && a>c){
+       printf("This is the maximum point value a=%d",a);
+     
+   }
+   else if(b>c && b>a){
+       printf("This is the maximum point value b=%d",b);
+   }
+   else{
+       printf("This is the maximum point value c=%d",c);
+   }
+       
+   
+}
