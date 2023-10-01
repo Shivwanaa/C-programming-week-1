0a1650c79ff6d59ac1ee0be009617a5019e65b5c
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-09-30 22:42:37 -0700

    Create fibbanocci.c

---
+++ fibbanocci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{ 
+   int n,n3;
+   scanf("%d",&n);
+   int n1=0;
+   int n2=1;
+   printf(" %d %d",n1,n2);
+   
+   for(int i=3;i<=n;i++){
+       
+       n3=n1+n2;    
+       printf(" %d",n3);    
+       n1=n2;    
+       n2=n3;    
+       
+   
+   }
+   
+   
+   
+   
+   
+   
+}
