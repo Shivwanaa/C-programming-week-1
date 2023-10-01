a511a32c6904601dc8018d16c7353c268a3f1d87
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-09-30 22:46:10 -0700

    Create sumofFirstLastdigit.c

---
+++ sumofFirstLastdigit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{   
+    int ldigit,n,a;
+    int sum=0;
+    
+    scanf("%d",&n);
+    ldigit=n%10;
+    while(n!=0){
+        a=n%10;
+        
+        n=n/10;
+        
+        
+        
+        
+        
+    }
+    sum=a+ldigit;
+    printf("sum of the digits is %d",sum);
+    
+    
+       
+   
+   
+   
+   
+   
+   
+   
+   
+}
