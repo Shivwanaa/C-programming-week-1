a4998277aa32c17a8198a602c70b8850fab406e7
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-04 03:14:34 -0700

    Create arr21.c

---
+++ arr21.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int arr[3][3];
+	for(int i=0;i<3;i++){
+	    printf("the row elements");
+	    for(int j=0;j<3;j++)
+		      scanf("%d",&arr[i][j]);
+	}
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++)
+		{
+		   printf("%d",arr[i][j]);
+		
+		}
+		printf("\n");
+	}
+}
