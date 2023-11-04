8911f041a78520377dd0d101e12c4047a90e91bc
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-04 03:18:53 -0700

    Create arr34.c

---
+++ arr34.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int s,m=0;
+    for(int i=0;i<n/2,i;i++){
+        m=m+arr[i];}
+        
+    for(int j=0;n/2<j<n;j++){
+            s=s+arr[j];
+        }
+    int a=s-m;
+    printf("%d",a);
+        
+        
+            
+        
+        
+        
+        
+           
+    
+}
