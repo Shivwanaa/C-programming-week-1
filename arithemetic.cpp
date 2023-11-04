7d846bcd29df660dc6b440c31e97186e75fe903c
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-04 03:55:59 -0700

    Update arithemetic.cpp

--- arithemetic.cpp
+++ arithemetic.cpp
@@ -1,16 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int a,b,add,sub,div,mul;
+	int a,b,add,sub,div,mul,modulus;
 	scanf("%d%d",&a,&b);
 	add=a+b;
 	sub=a-b;
 	div=a/b;
 	mul=a*b;
+	modulus=a%b;
 	printf("addition is %d:",add);
 	printf("subraction is %d:",sub);
 	printf("division is %d:",div);
 	printf("multiplication is %d:",mul);
+	printf("the remainder is %d:",modulus);
 	
 
 
