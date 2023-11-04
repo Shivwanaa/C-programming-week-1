9946f139a9382767031e3f2b1a9d3105c5ddfcb8
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-04 03:08:34 -0700

    Create arr13.c

---
+++ arr13.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main(){
+	int n,s;
+	int c=0;
+	printf("The no of elements: ");
+	scanf("%d",&n);
+	printf("the sum ");
+	scanf("%d",&s);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	for(int i=0;i<n;i++){
+	    for(int j=i+1;j<n;j++){
+	        if(arr[i]+arr[j]==s){
+	            printf("%d %d\n",arr[i],arr[j]);
+	            printf("True");
+	            c++;
+	            
+	        }
+	        
+	    }
+	    if(c>0){
+	        break;
+	    }
+	    
+	    }
+	    if(c==0){
+	        printf("false");
+	}
+	
+			
+		
+		
+		
+	
+	
+				
+	
+	}
