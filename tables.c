366fdf13bdf703c644486c86b974dcb5384b9223
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-05 12:16:15 -0800

    Create tables.c

---
+++ tables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int n,m;
+	printf("the table of :");
+	scanf("%d",&n);
+	for(int i=1;i<11;i++){
+	    m=1;
+	    m=i*n;
+	    printf("%d*%d=%d\n",n,i,m);
+		
+			
+			
+	}
+}
