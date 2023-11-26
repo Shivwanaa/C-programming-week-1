f9339d94c2f4fa5740b5fb08144e432620a9495e
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-11-26 02:45:17 -0800

    Update str12.cpp

--- str12.cpp
+++ str12.cpp
@@ -1,24 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 int main(){
 	char s[100];
-	scanf("%s",s);
-    int max,c;
+    scanf("%s",s);
+    int b=strlen(s);
+    int max=0;
+    int min=b;
+    int c,maxi,mini;
 	for(int i=0;s[i]!='\0';i++){
           c=0;
 		  for(int j=0;s[j]!='\0';j++){
-		  	c++;
-		  }
-		  max=0;
-		  if(max<c){
-		  	max=c;
-		  	
+		      if(s[i]==s[j]){
+		  	c++;}
 		  }
 		  
+		  if(max<c){
+		  	max=c;
+		  	maxi=i;
 		  	
 		  }
-		  printf("%d",max);
-
-		  		
-		
-	
+		  else if(min<b){
+		      min=c;
+		      mini=i;
+		  }
+	}
+		  printf("%c %c",s[maxi],s[mini]);
+    
 }
