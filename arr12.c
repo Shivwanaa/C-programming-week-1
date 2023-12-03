0a4db40cbc4e188380aae57262f85b2082296768
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-12-02 23:35:19 -0800

    Update arr12.c

--- arr12.c
+++ arr12.c
@@ -1,37 +1,26 @@
 #include <stdio.h>
 int main(){
-	int n;
-	int c;
-	int a;
+	int n,a;
 	printf("The no of elements: ");
 	scanf("%d",&n);
+	int s=0,m=0;
+	int c=0;
 	int arr[n];
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
-	for(int i=0;i<n;i=i+c+1){
+	for(int i=0;i<n;i++){
 	    c=0;
-		for(int j=i+1;j<n;j++)
-		{
-		    if(arr[i]==arr[j])
-			c++;
-			
-			
-		}
-			
-			
-			
-		if(c==0){
-			    printf("%d",arr[i]);
-			    break;
+		for(int j=0;j<n;j++){
+			if(arr[i]==arr[j]){
+			    c++;
 			    
 			}
-			
-		
-		
-		
+	}
+	if(c==1){
+	    printf("%d ",arr[i]);
+	    break;
 	}
 	
-				
-	
-	}
+}
+}
