8527d538dc53a8ae515bc901b2e1042c8e85fdc7
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-12-02 21:29:28 -0800

    Update Pattern 21.cpp

--- Pattern 21.cpp
+++ Pattern 21.cpp
@@ -3,6 +3,7 @@ int main(){
 	int n;
 	int k;
 	scanf("%d%d",&n,&k);
+	int t=k;
 	for(int i=0;i<n;i++){
 		for(int s=0;s<=i;s++){
 			printf("%d",k);
@@ -12,16 +13,15 @@ int main(){
 	k++;
 	printf("\n");
 	}
-	
-	int a=k+n-1;
-	
+	int a=t+n-1;
 	for(int i=0;i<n;i++){
 		
 		for(int s=0;s<n-i;s++){
 			
 			printf("%d",a);
-			a--;
+			
 		}
+		a--;
 		printf("\n");
 	}
 }
