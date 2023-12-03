f2c9658b31c5d48d1c2279a1235455318f9f921c
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-12-02 23:30:02 -0800

    Update arr11 act.cpp

--- arr11 act.cpp
+++ arr11 act.cpp
@@ -1,35 +1,30 @@
 #include <stdio.h>
 int main(){
-	int n;
+	int n,a;
 	printf("The no of elements: ");
 	scanf("%d",&n);
 	int s=0,m=0;
+	int c=0;
 	int arr[n];
-	int a=0;
 	for(int i=0;i<n;i++){
 		scanf("%d",&arr[i]);
 	}
 	for(int i=0;i<n;i++){
+	    c=0;
 		for(int j=i+1;j<n;j++){
 			if(arr[i]==arr[j]){
-			
-				a=arr[i];
-				break;
-				
-				
-				
+			    printf("%d ",arr[i]);
+			    c++;
+			    break;
+			    
 			}
-			
-			
-			}
-		
-		
-		
 	}
-	printf("The element that is first repeated %d",a);
-				
+	if(c==1){
+	    break;
+	}
 	
-	}
+}
+}
 	
 	
 
