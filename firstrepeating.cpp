ded52ff1821700b795c31f769827ebd0c999a2d5
108123116 <144584414+Shivwanaa@users.noreply.github.com>
2023-12-30 02:20:39 -0800

    Create firstrepeating.cpp

---
+++ firstrepeating.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int c=0;
+    
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+           if(a[i]==a[j]){
+              cout<<a[i]<<endl;
+              c=1;
+              break;
+            
+        }
+        
+        }
+        if(c==1){
+            break;
+        }
+    }
+}
